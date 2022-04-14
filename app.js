8722940c3260c6603fcda013b1b95d73a7af74f1
Czerwcuu <czerwiecr1999@gmail.com>
2022-04-14 20:32:58 +0200

    environement variable for db and port added

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ import express from "express"
 import mongoose from "mongoose"
 import cors from "cors"
 import taskRoutes from "./routes/tasks.js"
+import 'dotenv/config'
 
 const app = express()
 
+
 //Middlewares
+
 app.use(cors())
 app.use(express.json())
 app.use("/tasks", taskRoutes)
@@ -14,8 +17,8 @@ app.use("/tasks", taskRoutes)
 app.get("/", (req, res) => res.send())
 
 //Connect to DB
-const CONNECTION_URL = ""
-const PORT = process.env.PORT || 5000
+const CONNECTION_URL = process.env.DB_CONNECTION;
+const PORT = process.env.PORT;
 
 mongoose
   .connect(CONNECTION_URL, { useNewUrlParser: true })

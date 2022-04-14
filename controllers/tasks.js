71c71f95a43e1cff53ec4870417365655fffb827
Czerwcuu <czerwiecr1999@gmail.com>
2022-04-15 01:28:10 +0200

    edit and remove task route added

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -30,4 +30,44 @@ export const createTask = async (req, res) => {
   }
 }
 
+export const getOneTask = async (req, res) => {
+  try{
+    const task = await TaskModel.findById(req.params.taskId);
+    res.json(task);
+  }
+  catch (error) {
+    res.status(400).json({ message: error.message })
+  }
+}
+
+export const editTask = async (req, res) => {
+  try{
+     const task =  await TaskModel.updateOne({_id: req.params.taskId}, 
+      {$set : {
+      title: req.body.title,
+      priority: req.body.priority,
+      description: req.body.description,
+      attachments: req.body.attachments,
+      tags: req.body.tags,
+      status: req.body.status,
+    }});
+    res.json(task);
+  }
+  catch (error) {
+    res.status(400).json({ message: error.message })
+  }
+}
+
+export const deleteTask = async (req, res) => {
+  try{
+    const task = await TaskModel.findById(req.params.taskId);
+    await TaskModel.deleteOne({_id:req.params.taskId});
+
+    res.json(task);
+ }
+ catch (error) {
+   res.status(400).json({ message: error.message })
+ }
+}
+
 export default router
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,11 +1,17 @@
 import express from "express";
 
-import { getTasks, createTask } from "../controllers/tasks.js";
+import { getTasks, createTask, editTask, deleteTask, getOneTask } from "../controllers/tasks.js";
 
 const router = express.Router();
 
 router.get("/", getTasks);
 
+router.get("/:taskId", getOneTask);
+
 router.post("/", createTask);
+
+router.patch("/:taskId",editTask);
+
+router.delete("/:taskId",deleteTask);
 
 export default router;

140a0bd081345dae7ba6339417307f29a3367be9
Czerwcuu <czerwiecr1999@gmail.com>
2022-04-24 20:05:36 +0200

    createdAt, dueDate added to Task model

--- models/taskModel.js
+++ models/taskModel.js
@@ -2,12 +2,14 @@ import mongoose from "mongoose"
 
 const taskSchema = mongoose.Schema({
   title: String,
-  priopity: Number,
+  priority: Number,
   description: String,
   attachments: String,
   tags: String,
   status: Boolean,
   updated: { type: Date, default: Date.now },
+  createdAt: { type: Date, default: Date.now },
+  dueDate: {type: Date},
 })
 
 var TaskModel = mongoose.model("TaskModel", taskSchema)

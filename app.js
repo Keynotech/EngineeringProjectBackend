e639adaf6db91c07179e6cb00e2617075e69e4f3
czerwiecr <czerwiecr1999@gmail.com>
2022-07-06 20:35:13 +0200

    initial auth-firebase

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ import fileRoutes from "./files/fileRouter.js"
 import projectRoutes from "./projects/projectRouter.js"
 import folderRoutes from "./folders/folderRouter.js"
 
+//import { getToken } from "./firebase-auth-middleware.js"
+import admin from "firebase-admin";
+var serviceAccount = './jettasks.json';
+
 import "dotenv/config"
 
 const app = express()
@@ -15,6 +19,32 @@ const app = express()
 //Middlewares
 
 app.use(cors())
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const myLogger = async function(req,res,next){
+  console.log("XDDD");
+  const token = req.headers.authorization;
+  console.log(token);
+  try{
+
+  const decodeValue = await admin.auth().verifyIdToken(token);
+  if(decodeValue){
+    console.log(decodeValue);
+      return next();
+  }
+
+  return res.json({message:"Authorization..."});
+              
+}catch(e){
+  return res.json({message:"Authorization error!"});
+}
+}
+
+app.use(myLogger);
+
+
 app.use(express.json())
 app.use("/tasks", taskRoutes)
 app.use("/tags", tagRoutes)
@@ -24,11 +54,10 @@ app.use("/files", fileRoutes)
 app.use("/projects", projectRoutes)
 app.use("/folders", folderRoutes)
 
+
 // path file userid == userid
 app.use("/tasks/:taskId/:fileId", express.static("uploads"))
 
-//Routes
-app.get("/", (req, res) => res.send())
 
 //Connect to DB
 const CONNECTION_URL = process.env.DB_CONNECTION
